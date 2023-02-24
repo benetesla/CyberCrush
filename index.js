807ba88fdd0cb7e4d1360eda0132dc3971c69cd9
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-02-24 10:42:10 -0300

    conexão como o banco de dados e configuracoes do dotenv, usando o express como midlleware da  nossa aplicação

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const mongoose = require('mongoose');
+require("dotenv").config();
+
+
+mongoose.connect(process.env.MONGO_URI, {
+     useNewUrlParser: true, useUnifiedTopology: true 
+    }).then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.log(err));
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
